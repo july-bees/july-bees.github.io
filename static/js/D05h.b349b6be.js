153dbe28e5f8584397c13e6c43a7d8a553bc1aa8
july <july.scott@gmail.com>
2019-01-04 10:18:42 +0000

    1st round test.

---
+++ static/js/D05h.b349b6be.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([["D05h"],{D05h:function(e,n,o){"use strict";o.r(n);var s={name:"GoodsList",components:{GoodsQuery:o("Q7pY").a}},t=o("KHd+"),u=Object(t.a)(s,function(){var e=this.$createElement;return(this._self._c||e)("goods-query")},[],!1,null,null,null);u.options.__file="list.vue";n.default=u.exports}}]);
